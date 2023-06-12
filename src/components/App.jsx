6d4ca622474fcc64b7d2735b05a00522612998b3
Waldemar Kubiak <kubiak.waldemar@gmail.com>
2023-06-12 21:40:11 +0200

    phonebook saved into local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,14 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
   changeFilter = e => {
     const { name, value } = e.currentTarget;
     this.setState({ [name]: value });
@@ -45,6 +53,13 @@ export class App extends Component {
       };
     });
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+      // console.log(this.state.contacts);
+    }
+  }
 
   render() {
     return (
